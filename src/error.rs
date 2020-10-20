9bc4a8efd479ccdc863a4d492798c872d3e04d81
Clément Renault <clement@meilisearch.com>
2020-10-20 15:12:05 +0200

    Rework the Error::Merge variant

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,8 @@
+use std::convert::Infallible;
 use std::{fmt, io, error};
 
 #[derive(Debug)]
-pub enum Error<U=()> {
+pub enum Error<U=Infallible> {
     Io(io::Error),
     Merge(U),
     InvalidCompressionType,
@@ -11,22 +12,23 @@ impl<U> Error<U> {
     pub(crate) fn convert_merge_error<V>(self) -> Error<V> {
         match self {
             Error::Io(io) => Error::Io(io),
+            Error::InvalidCompressionType => Error::InvalidCompressionType,
             _ => panic!("cannot convert a merge error"),
         }
     }
 }
 
-impl fmt::Display for Error {
+impl<U: fmt::Display> fmt::Display for Error<U> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::Io(io) => write!(f, "{}", io),
-            Error::Merge(_) => f.write_str("<user merge error>"),
+            Error::Merge(e) => write!(f, "merge error: {}", e),
             Error::InvalidCompressionType => f.write_str("invalid compression type"),
         }
     }
 }
 
-impl error::Error for Error { }
+impl<U: fmt::Display + fmt::Debug> error::Error for Error<U> { }
 
 impl<U> From<io::Error> for Error<U> {
     fn from(err: io::Error) -> Error<U> {
