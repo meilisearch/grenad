34f5684ae78a84beadcdd33450d577db39b7882d
Kerollmops <clement@meilisearch.com>
2021-08-08 13:11:54 +0200

    Add documentation to the Merge trait

--- src/merge.rs
+++ src/merge.rs
@@ -1,7 +1,11 @@
+/// This trait defines the way multiple values must be merged.
 pub trait Merge {
+    /// The error that can be raised when merging the values.
     type Error;
+    /// The type that is returned by the merge method.
     type Output: AsRef<[u8]>;
 
+    /// The function that merges the values that correspond to the same key.
     fn merge<I, A>(&self, key: &[u8], values: I) -> Result<Self::Output, Self::Error>
     where
         I: IntoIterator<Item = A>,
